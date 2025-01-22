bfaf3cdd1b12d370549175784557a162e5343bf4
FurryGirl777 <amasipm@edu.tecnocampus.cat>
2025-01-22 16:21:10 +0100

    First Commit

---
+++ Assets/Scripts/ControlesPlayer.cs
@@ -0,0 +1,89 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ControlesPlayer : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    public float moveSpeed;
+    private Vector2 dir;
+    public float jumpForce;
+    private bool isGrounded;
+    
+
+    public int jumpCount;
+    public int maxJumps;
+    
+    private Animator animator;
+
+    public InputActionReference move;
+    public InputActionReference jump;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        dir = move.action.ReadValue<Vector2>();
+        animator.SetBool("isGrounded", isGrounded);
+
+        float speed = Mathf.Abs(dir.x); 
+        animator.SetFloat("Speed", speed);
+
+        if (dir.x > 0) 
+        {
+            transform.localScale = new Vector3(-1, 1, 1); 
+        }
+        else if (dir.x < 0) 
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(dir.x * moveSpeed, rb.linearVelocity.y);
+    }
+
+    private void OnEnable()
+    {
+        jump.action.started += Jump;
+    }
+
+    private void OnDisable()
+    {
+        jump.action.started -= Jump;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            jumpCount = 0; 
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+
+    private void Jump(InputAction.CallbackContext obj)
+    {
+        if (jumpCount <= maxJumps)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            jumpCount++;
+        }
+    }
+}
+ 
