543223471d17a00aebdb3fe8d9109266a510e1df
FurryGirl777 <amasipm@edu.tecnocampus.cat>
2025-01-26 21:17:43 +0100

    Audio

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -7,6 +7,13 @@ public class Score : MonoBehaviour
     public int punts;
     private Text scoreText;
 
+    private AudioSource meow;
+
+    private void Start()
+    {
+        meow = GetComponent<AudioSource>();
+    }
+
     private void Awake()
     {
         scoreText = GetComponent<Text>();
@@ -25,6 +32,7 @@ public class Score : MonoBehaviour
     private void UpdateScore(CoinPickup coin)
     {
         punts += coin.Value;
+        meow.Play();
         scoreText.text = "Score: " + punts.ToString();
     }
 }
